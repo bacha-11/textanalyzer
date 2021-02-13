b5c25f48260894cc4ab9a3560bb9b2143c26c624
bacha-11 <ijazbacha14@gmail.com>
2021-02-13 13:13:51 +0500

    add capitalize, upper, lower, printable functionality

--- app/routes.py
+++ app/routes.py
@@ -15,8 +15,41 @@ def index():
 @app.route('/find_length', methods=['GET', 'POST'])
 def find_length():
     if request.method == 'POST':
-        res = request.form['string']
-        return render_template('string.html', title='Length', res=res)
+        if request.form.get('find_length'):
+            res = request.form['string']
+            return render_template('string.html', title='Length', res=res)
+
+        elif request.form.get('capitalize'):
+            res = request.form['string']
+            return render_template('string.html', cap_str=res)
+            #return redirect(url_for('cap_string', res=res))
+
+        elif request.form.get('upper'):
+            res = request.form['string']
+            text_upper = res.upper()
+            return render_template('string.html', text_upper=text_upper)
+
+        elif request.form.get('lower'):
+            res = request.form['string']
+            text_lower = res.lower()
+            return render_template('string.html', text_upper=text_lower)
+
+        elif request.form.get('encode_text'):
+            res = request.form['string']
+            text_encode = res.encode()
+            return render_template('string.html', text_encode=text_encode)
+
+        elif request.form.get('print'):
+            res = request.form['string']
+            print_able = res.isprintable()
+            return render_template('string.html',  print_able=print_able, print=res)
+    return render_template('string.html')
+
+
+@app.route('/cap_string/<res>', methods=['POST', 'GET'])
+def cap_string(res):
+    if res:
+        return render_template('string.html', cap_str=res)
     return render_template('string.html')
 
 
@@ -27,7 +60,7 @@ def word_replace():
         old_word = request.form['old_word']
         new_word = request.form['new_word']
         return render_template('replace.html', title='Replace Word', all_string=all_string, old_word=old_word, new_word=new_word)
-
+        
     return render_template('replace.html', title='Replace Word')
 
 
